a5e614a17039854b62e4c07294bdbb8669d0be4b
HebertCordero <hebert.cordero@outlook.com>
2022-03-11 16:18:51 -0600

    new internStyles + script.js > NPS controllers

--- is/centro de ayuda/index.js
+++ is/centro de ayuda/index.js
@@ -11,15 +11,15 @@ $(document).ready(function () {
             $("#cx_cambios").addClass("active");
             break;
 
-        case "/envios-entregas":
+        case "/envios":
             $("#cx_envios").addClass("active");
             break;
 
-        case "/formas-pago":
+        case "/forma-de-pago":
             $("#cx_pagos").addClass("active");
             break;
 
-        case "/acerca-de-tu-cuenta":
+        case "/cuenta":
             $("#cx_tuCuenta").addClass("active");
             break;
 
@@ -30,8 +30,40 @@ $(document).ready(function () {
         default:
             break;
     }
+    
+    class CentroAyuda{
+        constructor(apiUrl) {
+            this.apiUrl = apiUrl;
+        }
+        getValue(data){
+            $.ajax({
+                url: this.apiUrl,
+                data: data,
+                type: "GET",
+                dataType: "json",
+                success: function(respuesta){
+                    //respuesta.id
+                    //respuesta.value
+                    console.log(respuesta);
+                }
+            });
+        }
+        setValue(data){
+            $.ajax({
+                url: this.apiUrl,
+                data: data,
+                type: "POST",
+                dataType: "json",
+                success: function(respuesta){
+                    //respuesta.id
+                    //respuesta.value
+                    console.log(respuesta);
+                }
+            });
+        }
+    }
+    centroAyuda = new CentroAyuda('https://iss3.innovasport.com/api/centro-ayuda')
 });
-
 cx_container = $(".cx-cdaiv__menu-options.active");
 $(".cx-cdaiv__menu-item").click(function (e) {
     var id = $(this).attr("id");
@@ -49,7 +81,6 @@ $(".cx-cdaiv__menu-item").click(function (e) {
         $(".cx-cdaiv__menu-wrapper").removeClass("active");
     }
 });
-
 $(".cx-cdaiv__menuMobile").click(function (e) {
     $(".cx-cdaiv__menu-options").addClass("active");
     $(".cx-cdaiv__menu-wrapper").addClass("active");
@@ -63,3 +94,32 @@ $(".cx-cdaiv__menu-wrapper").click(function (e) {
         $(".cx-cdaiv__menu-wrapper").removeClass("active");
     }
 });
+/* NPS */
+$('.cx-cdaiv__item-footer-icons').click(function (e) {
+    //console.log($(e.target.classList)[1]);
+    if ($(e.target.classList)[1] == "thumbs-up") {
+        $(e.target).parent().empty();
+        //console.log($(e.target).parent().siblings())
+        $(e.target).parent().parent().append("<h3 class='ux-btn-message'>¡Gracias! Estamos en constante mejora para ti</h3>");
+    } else if ($(e.target.classList)[1] == "thumbs-down") {
+        $(e.target).parent().parent().append("<form class='ux-form'><label class=''>¿Por qué?</label><input type='text' question='Pasos para compra' class='ux-campoMejora' id='ux-mejora' name='ux-mejora'><input type='submit' class='ux-nps-btn' value='Enviar' id='ux-enviar'></form>");
+        $(e.target).parent().empty();
+    }
+});
+$('.ux-nps-btn').click(function (e) {
+    console.log('done!');
+    e.preventDefault();
+    var input = $(e.target).closest('.ux-form').find('#ux-mejora');
+    var val = input.val();
+    var question = input.attr('question')
+    console.log(val, question);
+    data = {
+        'type': 'compras-linea',
+        'field': question,
+        'value': val,
+    }
+    row = centroAyuda.setValue(data);
+    var form = $(this).parent()
+    $(form).empty();
+    $(form).append("<h3 class='ux-btn-message'>¡Gracias! Estamos en constante mejora para ti</h3>");
+})
