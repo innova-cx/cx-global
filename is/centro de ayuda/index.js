47d21710606b3a3e3927d93371f3194a26b9d0bf
Hebert Cordero <heberto.cordero@outlook.com>
2022-03-23 12:01:02 -0600

    centro-de-ayuda is added prod script

--- is/centro de ayuda/index.js
+++ is/centro de ayuda/index.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
             $("#cx_compraEnLinea").addClass("active");
             break;
 
-        case "/cambios-devoluciones":
+        case "/cambios-y-devoluciones":
             $("#cx_cambios").addClass("active");
             break;
 
@@ -30,7 +30,7 @@ $(document).ready(function () {
         default:
             break;
     }
-    
+
     class CentroAyuda{
         constructor(apiUrl) {
             this.apiUrl = apiUrl;
@@ -44,7 +44,6 @@ $(document).ready(function () {
                 success: function(respuesta){
                     //respuesta.id
                     //respuesta.value
-                    console.log(respuesta);
                 }
             });
         }
@@ -57,41 +56,11 @@ $(document).ready(function () {
                 success: function(respuesta){
                     //respuesta.id
                     //respuesta.value
-                    console.log(respuesta);
                 }
             });
         }
     }
-    centroAyuda = new CentroAyuda('https://iss3.innovasport.com/api/centro-ayuda');
-    /* NPS */
-    $('body').on('click', '.cx-cdaiv__item-footer-icons', function(e) {
-        //console.log($(e.target.classList)[1]);
-        if ($(e.target.classList)[1] == "thumbs-up") {
-            //console.log($(e.target).parent().siblings())
-            $(e.target).parent().parent().append('<div class="alert alert-success" style="margin-top:0;margin-bottom:0">¡Gracias! Estamos en constante mejora para ti</div>');
-            $(e.target).parent().empty();
-        } else if ($(e.target.classList)[1] == "thumbs-down") {
-            $(e.target).parent().parent().append("<form class='ux-form'><label class=''>¿Por qué?</label><input type='text' question='Pasos para compra' class='ux-campoMejora' id='ux-mejora' name='ux-mejora'><div class='ux-nps-btn' id='ux-enviar'>Enviar</div></form>");
-            $(e.target).parent().empty();
-        }
-    });
-    $('body').on('click', '.ux-nps-btn', function(e) {
-        console.log('done!');
-        e.preventDefault();
-        var input = $(e.target).closest('.ux-form').find('#ux-mejora');
-        var val = input.val();
-        var type = $('.cx-cdaiv__body').attr('type');
-        var question = input.attr('question');
-        console.log(val, question);
-        data = {
-            'type': type,
-            'field': question,
-            'value': val,
-        }
-        row = centroAyuda.setValue(data);
-        var form = $(this).parent()
-        $(form).html('<div class="alert alert-success" style="margin-top:0;margin-bottom:0">Gracias por tu opinión</div>');
-    })
+    centroAyuda = new CentroAyuda('https://iss3.innovasport.com/api/centro-ayuda')
 });
 cx_container = $(".cx-cdaiv__menu-options.active");
 $(".cx-cdaiv__menu-item").click(function (e) {
@@ -123,3 +92,31 @@ $(".cx-cdaiv__menu-wrapper").click(function (e) {
         $(".cx-cdaiv__menu-wrapper").removeClass("active");
     }
 });
+/* NPS */
+$('.cx-cdaiv__item-footer-icons').click(function (e) {
+    //console.log($(e.target.classList)[1]);
+    if ($(e.target.classList)[1] == "thumbs-up") {
+        $(e.target).parent().empty();
+        //console.log($(e.target).parent().siblings())
+        $(e.target).parent().parent().append("<h3 class='ux-btn-message'>¡Gracias! Estamos en constante mejora para ti</h3>");
+    } else if ($(e.target.classList)[1] == "thumbs-down") {
+        $(e.target).parent().parent().append("<form class='ux-form'><label class=''>¿Por qué?</label><input type='text' question='Pasos para compra' class='ux-campoMejora' id='ux-mejora' name='ux-mejora'><input type='submit' class='ux-nps-btn' value='Enviar' id='ux-enviar'></form>");
+        $(e.target).parent().empty();
+    }
+});
+$('.ux-nps-btn').click(function (e) {
+    console.log('done!');
+    e.preventDefault();
+    var input = $(e.target).closest('.ux-form').find('#ux-mejora');
+    var val = input.val();
+    var question = input.attr('question')
+    console.log(val, question);
+    data = {
+        'type': 'compras-linea',
+        'field': question,
+        'value': val,
+    }
+    row = centroAyuda.setValue(data);
+    var form = $(this).parent()
+    $(form).empty();
+})
