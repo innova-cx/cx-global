c1415678295b4dbda86b33fb688e3fad515e8c7c
jcarloscasasola <carlos.casasola@innovasport.com>
2026-08-07 15:42:06 -0600

    Create academias-kits.js

---
+++ is/ecomm/academias-kits.js
@@ -0,0 +1,38 @@
+var kitsArray = {
+        '000000000000402000': {
+            name: 'Kit Entrenador Juego',
+            imagePLP: 'https://www.innovasport.com/medias/Kit-Entrenador-Juego-300x300.jpg?context=bWFzdGVyfHJvb3R8Njg2M3xpbWFnZS9qcGVnfGFEZGtMMmhpWXk4eE5USTNORFU1TkRFd056UXlNaTlMYVhRdFJXNTBjbVZ1WVdSdmNpMUtkV1ZuYnkwek1EQjRNekF3TG1wd1p3fDBkNGY3ZjVjNGM2ZDIwMWQ3NGI4YjVjYmZmNjE4NjQ3MjA5NWI1MGVlZTFiZDMwMjgzMTY2M2VmNTZkZDI2ZGQ',
+            imagePDP: 'https://www.innovasport.com/medias/Kit-Entrenador-Juego-1200x1200.jpg?context=bWFzdGVyfHJvb3R8NDU1NzF8aW1hZ2UvanBlZ3xhRGRpTDJoaVppOHhOVEkzTkRVNU5EQTBNVGc0Tmk5TGFYUXRSVzUwY21WdVlXUnZjaTFLZFdWbmJ5MHhNakF3ZURFeU1EQXVhbkJufGZlMzZlZjIyNDZmNzg5MGUyMjgwN2FiYmY0MmI5NmJkNjg1ZmM1Njc3MjZjNjViNTUwMTYzZmI3ZmVjYjc4ZGY',
+            price:'1,987'
+        },
+        '000000000000402006': {
+            name: 'Kit Entrenador Viaje',
+            imagePLP: 'https://www.innovasport.com/medias/Kit-Entrenador-Viaje-300x300.jpg?context=bWFzdGVyfHJvb3R8Njk2MHxpbWFnZS9qcGVnfGFHSmxMMmhpT0M4eE5USTNORFU1TkRJek9EUTVOQzlMYVhRdFJXNTBjbVZ1WVdSdmNpMVdhV0ZxWlMwek1EQjRNekF3TG1wd1p3fDgwYzk0MzJkYTQ5MDUwN2MyYjliZGYwMDI1ZDhmYWYyZjM2YzFkMzdjZWQzNjU3YWU5ZDU4YzQ5NzgxM2U3NGQ',
+            imagePDP: 'https://www.innovasport.com/medias/Kit-Entrenador-Viaje-1200x1200.jpg?context=bWFzdGVyfHJvb3R8NDI2ODN8aW1hZ2UvanBlZ3xhR0pqTDJoaVlpOHhOVEkzTkRVNU5ERTNNamsxT0M5TGFYUXRSVzUwY21WdVlXUnZjaTFXYVdGcVpTMHhNakF3ZURFeU1EQXVhbkJufGFlYTgyOTkwZTBhNzVmNzc4N2Y1Nzk0MzI2MzFlNTgwNTQxMmQ4YmY1ZGY2ZGZjMGZlMTg4ODZkMDZlNDAzZWY',
+            price:'1,838'
+        },
+        '000000000000401992': {
+            name: 'Kit Jugador Jóvenes',
+            imagePLP: 'https://www.innovasport.com/medias/Kit-Jugador-Jovenes-300x300.jpg?context=bWFzdGVyfHJvb3R8MTA0MzF8aW1hZ2UvanBlZ3xhR00zTDJobU5TOHhOVEkzTkRVNU16azNOak0xTUM5TGFYUXRTblZuWVdSdmNpMUtiM1psYm1WekxUTXdNSGd6TURBdWFuQm58NzQ4M2FjMmNjMjVhZTIyYTMwZjI4MjI5MjQ0YWFhZGUyM2YxODRlYTIxYzk2YmYwNjVlOGY3OTgzMWY0ZWZjNQ',
+            imagePDP: 'https://www.innovasport.com/medias/Kit-Jugador-Jovenes-1200x1200.jpg?context=bWFzdGVyfHJvb3R8NjY3MzZ8aW1hZ2UvanBlZ3xhREU1TDJobU5TOHhOVEkzTkRVNU16a3hNRGd4TkM5TGFYUXRTblZuWVdSdmNpMUtiM1psYm1WekxURXlNREI0TVRJd01DNXFjR2N8YmNhMWEzZjQ3MDEyZDUxYWNiMzA5NzdiNDQ2N2MzOWEwYzlkYjY2MTUxZDQ0NWQ5MjUzMDkxYWM1NTI2N2ZiZA',
+            price:'3,323'
+        },
+        '000000000000401986': {
+            name: 'Kit Jugador Niño',
+            imagePLP: 'https://www.innovasport.com/medias/Kit-Jugador-Nin-o-300x300.jpg?context=bWFzdGVyfHJvb3R8MTAxNzJ8aW1hZ2UvanBlZ3xhRE00TDJobFlpOHhOVEkzTkRVNU16WTBPRFkzTUM5TGFYUXRTblZuWVdSdmNpMU9hVzdNZzI4dE16QXdlRE13TUM1cWNHY3w1OGVlZTAxMmQzNDM2ZmJjNjdlNGE3YmM3ZTNkZDE4NWQxNzA1YTIxNDJjMGFmNDQ1MzkwNmI4N2RmOWI5YmY3',
+            imagePDP: 'https://www.innovasport.com/medias/Kit-Jugador-Nin-o-1200x1200.jpg?context=bWFzdGVyfHJvb3R8NjUzMzl8aW1hZ2UvanBlZ3xhR1F5TDJobE5DOHhOVEkzTkRVNU16UTRORGd6TUM5TGFYUXRTblZuWVdSdmNpMU9hVzdNZzI4dE1USXdNSGd4TWpBd0xtcHdad3w0ZTU4Y2JiMGFmNTM0NzM4MDQ3N2EyNGVhZDI1NzNlYmY0NjYyMTEyNTZlMzI5MDI5YjM0ZjNmNWVmMWFlZjVm',
+            price:'2,723'
+        },
+        '000000000000410055': {
+            name: 'Kit Medias Adulto',
+            imagePLP: 'https://www.innovasport.com/medias/Kit-Medias-Adulto-300x300.jpg?context=bWFzdGVyfHJvb3R8Nzg1MXxpbWFnZS9qcGVnfGFHVmtMMmhpTVM4eE5USTNORFU1TkRRek5URXdNaTlMYVhRdFRXVmthV0Z6TFVGa2RXeDBieTB6TURCNE16QXdMbXB3Wnd8ZmNjNzExNGQ1OTAwMzhmNWE0MzE4MjIzODAwNjhmMjJkZjgxYzZiYjEzM2U3ZjBkYTg0NzkyNzVhMDA3M2E2OA',
+            imagePDP: 'https://www.innovasport.com/medias/Kit-Medias-Adulto-1200x1200.jpg?context=bWFzdGVyfHJvb3R8NjM5OTB8aW1hZ2UvanBlZ3xhR1JqTDJoaFpTOHhOVEkzTkRVNU5EVXdNRFl6T0M5TGFYUXRUV1ZrYVdGekxVRmtkV3gwYnkweE1qQXdlREV5TURBdWFuQm58MjljNjNkYTAxYzRhMzQ5ZTZhZjFkZDRkMzI4ZGVjZmJmN2I4MGExZjE3NzkzYzY5MjgzMzczY2Y2NTYyMWUwZQ',
+            price:'138'
+        },
+        '000000000000410077': {
+            name: 'Kit Medias Niño',
+            imagePLP: 'https://www.innovasport.com/medias/Kit-Medias-Nin-o-300x300.jpg?context=bWFzdGVyfHJvb3R8NjM4M3xpbWFnZS9qcGVnfGFHRmpMMmhpTlM4eE5USTNORFU1TkRNd05EQXpNQzlMYVhRdFRXVmthV0Z6TFU1cGJzeURieTB6TURCNE16QXdMbXB3Wnd8YzQ0MGI2MDhmODY2OWQ0ZDRkOWQwODNlZDVjYjA2YWY1MmI3MmMzYjk3MzIxYjI0OGE1OGRlMDVjYmI2OTFlYw',
+            imagePDP: 'https://www.innovasport.com/medias/Kit-Medias-Nin-o-1200x1200.jpg?context=bWFzdGVyfHJvb3R8NDk5ODZ8aW1hZ2UvanBlZ3xhR1ptTDJoaU5DOHhOVEkzTkRVNU5ETTJPVFUyTmk5TGFYUXRUV1ZrYVdGekxVNXBic3lEYnkweE1qQXdlREV5TURBdWFuQm58NjY4OGM3MmMwNDZhZTZkNmRiMTdjZWIwNzI1M2I2Zjk1NmEyYWIyMjYzMzk0MzUwNzE0ZjNjMGZlOWQ0Y2NkMw',
+            price:'138'
+        }
+    };
