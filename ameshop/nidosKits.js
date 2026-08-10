0c80be590cdc4a8aba0ebc4e1ffe77d0701942a7
cmvinnovasport <carlos.martinezv@innovasport.com>
2026-08-10 13:25:17 -0600

    Create nidosKits.js

---
+++ ameshop/nidosKits.js
@@ -0,0 +1,32 @@
+var kitsArray = {
+  "000000000000423192": {
+    name: "Kit Jugador Infantil",
+    imagePDP:
+      "https://site.ameshop.com.mx/cx/lp%20teamsports/kits-3/kit-jugador-infantil-1200x780.jpg",
+    price: "4,144",
+  },
+  "000000000000423198": {
+    name: "Kit Portero Infantil",
+    imagePDP:
+      "https://site.ameshop.com.mx/cx/lp%20teamsports/kits-3/kit-portero-infantil-1200x780.jpg",
+    price: "4,144",
+  },
+  "000000000000423190": {
+    name: "Kit Jugador Juvenil",
+    imagePDP:
+      "https://site.ameshop.com.mx/cx/lp%20teamsports/kits-3/kit-jugador-hombre-1200x780.jpg",
+    price: "4,644",
+  },
+  "000000000000423196": {
+    name: "Kit Portero Juvenil",
+    imagePDP:
+      "https://site.ameshop.com.mx/cx/lp%20teamsports/kits-3/kit-portero-hombre-1200x780.jpg",
+    price: "4,644",
+  },
+  "000000000000394093": {
+    name: "Kit Coach",
+    imagePDP:
+      "https://site.ameshop.com.mx/cx/lp%20teamsports/kits-3/kit-coaches-1200x780.jpg",
+    price: "3,945",
+  },
+};
